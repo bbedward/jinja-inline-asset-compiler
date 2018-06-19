2dc774f9b8e12c03dffabca046f26aef6144d507
Brandon Berhent <bbedward@gmail.com>
2018-06-19 10:55:24 -0400

    Python 3 stuff

--- jiac/extension.py
+++ jiac/extension.py
@@ -10,7 +10,7 @@ class CompilerExtension(Extension):
     tags = set(['compile'])
 
     def parse(self, parser):
-        lineno = parser.stream.next().lineno
+        lineno = next(parser.stream).lineno
         body = parser.parse_statements(['name:endcompile'], drop_needle=True)
 
         if len(body) > 1:
